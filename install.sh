8d40beee031e36f7b3f6adcf90eacf4da1e58e2e
Martin Frnka <martin.frnka@gmail.com>
2013-11-18 13:58:41 +0100

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#sudo apt-get update -y
+#sudo apt-get upgrade -y
+
+sudo apt-get install python-software-properties
+
+#sudo add-apt-repository ppa:gnome3-team/gnome3
+
+#sudo apt-get update
+sudo apt-get install gnome-shell gdm gnome-terminal -y
+
+
