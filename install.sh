4204efa675cae0a1c8e54da89f32feb7d4030536
Martin Frnka <martin.frnka@gmail.com>
2013-11-18 14:06:18 +0100

    modified  install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,8 @@
 #sudo apt-get update -y
 #sudo apt-get upgrade -y
 
-sudo apt-get install python-software-properties
-
-#sudo add-apt-repository ppa:gnome3-team/gnome3
-
-#sudo apt-get update
-sudo apt-get install gnome-shell gdm gnome-terminal -y
+sudo apt-get install gdm -y
+sudo apt-get install gnome-session -y
+sudo apt-get install gnome-terminal -y
 
 
